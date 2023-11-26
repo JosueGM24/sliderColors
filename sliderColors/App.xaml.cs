7e61b22175ab29561a5ebef1df133a0daea8439f
Daniel <jos.god.mel.10@gmail.com>
2023-11-25 22:49:31 -0600

    Agregar archivos de proyecto.

---
+++ sliderColors/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace sliderColors
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ sliderColors/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace sliderColors
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Red_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Color color = new Color(((float)Red.Value), ((float)Green.Value), ((float)Blue.Value));
+            boxColor.Color = color;
+            nameColor.Text = color.ToArgbHex();
+            nameColor.TextColor = color;
+        }
+
+        private void Green_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Color color = new Color(((float)Red.Value), ((float)Green.Value), ((float)Blue.Value));
+            boxColor.Color = color;
+            nameColor.Text = color.ToArgbHex();
+            nameColor.TextColor = color;
+        }
+
+        private void Blue_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Color color = new Color(((float)Red.Value), ((float)Green.Value), ((float)Blue.Value));
+            boxColor.Color = color;
+            nameColor.Text = color.ToArgbHex();
+            nameColor.TextColor = color;
+        }
+    }
+}
